2a5ba0ce3a50c9ac8c538e07c8b2ac6f53b673a9
nadin <meykemoningka02@gmail.com>
2025-11-26 13:26:14 +0800

    Update kelompok6-kalkulator.py

--- kelompok6-kalkulator.py
+++ kelompok6-kalkulator.py
@@ -1,34 +1,32 @@
-# Bagian Gerry: Desain UI, konfigurasi window, tampilan label
+# Bagian Meike: Pembuatan tombol dan pengaturan posisi
 
-import tkinter
+for row in range(row_count):
+    for column in range(column_count):
+        value = button_values[row][column]
 
-button_values = [
-    ["AC", "+/-", "%", "÷"], 
-    ["7", "8", "9", "×"], 
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", ".", "√", "="]
-]
+        button = tkinter.Button(frame, text=value, font=("Arial", 30),
+                                width=column_count - 1, height=1,
+                                command=lambda value=value: button_clicked(value))
 
-right_symbols = ["÷", "×", "-", "+", "="]
-top_symbols = ["AC", "+/-", "%"]
+        if value in top_symbols:
+            button.config(foreground=color_black, background=color_light_gray)
+        elif value in right_symbols:
+            button.config(foreground=color_white, background=color_orange)
+        else:
+            button.config(foreground=color_white, background=color_dark_gray)
 
-row_count = len(button_values)
-column_count = len(button_values[0])
+        button.grid(row=row + 1, column=column)
 
-color_light_gray = "#D4D4D2"
-color_black = "#1C1C1C"
-color_dark_gray = "#505050"
-color_orange = "#FF9500"
-color_white = "white"
+frame.pack()
 
-window = tkinter.Tk()
-window.title("Calculator")
-window.resizable(False, False)
+# Posisi window di tengah layar
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
-frame = tkinter.Frame(window)
-label = tkinter.Label(frame, text="0", font=("Arial", 45),
-                      background=color_black, foreground=color_white,
-                      anchor="e", width=column_count)
-
-label.grid(row=0, column=0, columnspan=column_count, sticky="we")
+window.mainloop()
