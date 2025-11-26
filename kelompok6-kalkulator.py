71fe1a115deef6de50d70423a97aacfa35fade1f
snake <bawoelgerry10@gmail.com>
2025-11-26 13:15:19 +0800

    Update kelompok6-kalkulator.py
    membuat design ui menggunakan fungsi tkinter

--- kelompok6-kalkulator.py
+++ kelompok6-kalkulator.py
@@ -1,1 +1,34 @@
+# Bagian Gerry: Desain UI, konfigurasi window, tampilan label
 
+import tkinter
+
+button_values = [
+    ["AC", "+/-", "%", "÷"], 
+    ["7", "8", "9", "×"], 
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "√", "="]
+]
+
+right_symbols = ["÷", "×", "-", "+", "="]
+top_symbols = ["AC", "+/-", "%"]
+
+row_count = len(button_values)
+column_count = len(button_values[0])
+
+color_light_gray = "#D4D4D2"
+color_black = "#1C1C1C"
+color_dark_gray = "#505050"
+color_orange = "#FF9500"
+color_white = "white"
+
+window = tkinter.Tk()
+window.title("Calculator")
+window.resizable(False, False)
+
+frame = tkinter.Frame(window)
+label = tkinter.Label(frame, text="0", font=("Arial", 45),
+                      background=color_black, foreground=color_white,
+                      anchor="e", width=column_count)
+
+label.grid(row=0, column=0, columnspan=column_count, sticky="we")
