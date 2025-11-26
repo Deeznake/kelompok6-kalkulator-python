b41e1e739540655cbcff58ead8fde3324a87a837
snake <bawoelgerry10@gmail.com>
2025-11-26 14:15:44 +0800

    Update kelompok6-kalkulator.py

--- kelompok6-kalkulator.py
+++ kelompok6-kalkulator.py
@@ -1,18 +1,1 @@
-# Bagian Kenzo: Variabel data global & fungsi utilitas
 
-A = "0"
-operator = None
-B = None
-
-def clear_all():
-    """Menghapus semua input dan mengatur ulang kalkulator"""
-    global A, B, operator
-    A = "0"
-    operator = None
-    B = None
-
-def remove_zero_decimal(num):
-    """Menghapus .0 di angka seperti 12.0 menjadi 12"""
-    if num % 1 == 0:
-        num = int(num)
-    return str(num)
